3b49713063c33d67de18b3aa97379d8ebac89a4e
Ricardo Bucho <ricardobucho@live.com>
2026-04-13 18:34:31 +0100

    Point cask at workers.dev URL for domain isolation

--- Casks/origami.rb
+++ Casks/origami.rb
@@ -2,13 +2,13 @@ cask "origami" do
   version "0.17.0"
   sha256 "25ac14ab9ad7ee916bc8b44f72c211a295af976e1c7a2f78447a983391d401c1"
 
-  url "https://storage.tryorigami.app/releases/Origami_#{version}_universal.dmg"
+  url "https://storage-worker.ricardo-020.workers.dev/releases/Origami_#{version}_universal.dmg"
   name "Origami"
   desc "Workspace-oriented terminal manager"
   homepage "https://tryorigami.app/"
 
   livecheck do
-    url "https://storage.tryorigami.app/releases/latest.json"
+    url "https://storage-worker.ricardo-020.workers.dev/releases/latest.json"
     strategy :json do |json|
       json["version"]
     end
