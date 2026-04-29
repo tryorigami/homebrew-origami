5ca359bcca80a35a9f3096bbde66e63e6dd31bb4
Ricardo Bucho <ricardobucho@live.com>
2026-04-29 23:54:22 +0100

    origami 0.18.0

--- Casks/origami.rb
+++ Casks/origami.rb
@@ -1,10 +1,10 @@
 cask "origami" do
-  version "0.17.1"
-  sha256 "bbc2187c5921207f1985cced548a65e46b7287e34c779f5030b21a5d1f72f381"
+  version "0.18.0"
+  sha256 "44c0ee687ee938377aa9eedd70fbedff83136673dadb822d97ec035d5bae1c96"
 
   url "https://storage-worker.ricardo-020.workers.dev/releases/Origami_#{version}_universal.dmg"
   name "Origami"
-  desc "Project-oriented terminal manager"
+  desc "Workspace-oriented terminal manager"
   homepage "https://tryorigami.app/"
 
   livecheck do
