6ed1403e80e6b362bd9ffca5d59e04eaae8bb832
Ricardo Bucho <ricardobucho@live.com>
2026-05-14 22:44:13 +0100

    origami 0.19.0

--- Casks/origami.rb
+++ Casks/origami.rb
@@ -1,6 +1,6 @@
 cask "origami" do
-  version "0.18.2"
-  sha256 "275e70886f1cac07f5049d6fc120bb5ab63ee398f06f58a9956a45882d399f2b"
+  version "0.19.0"
+  sha256 "8ee9d01bd8217063434380644581ac7daa49ab590a1d3737234cbebf8bd5934d"
 
   url "https://storage-worker.ricardo-020.workers.dev/releases/Origami_#{version}_universal.dmg"
   name "Origami"
